d76e3d7795e8731692ce45f4e118d62c920f636f
Baplar <baplar@gmail.com>
2025-07-23 00:16:50 +0200

    Use new generic function for load progress indication

--- ResoniteModLoader/LoadProgressIndicator.cs
+++ ResoniteModLoader/LoadProgressIndicator.cs
@@ -4,33 +4,11 @@ namespace ResoniteModLoader;
 
 // Custom LoadProgressIndicator logic failing shouldn't stop the rest of the modloader.
 internal static class LoadProgressIndicator {
-	private static bool failed;
-
-	private static FieldInfo? _showSubphase;
-	private static FieldInfo? ShowSubphase {
-		get {
-			if (_showSubphase is null) {
-				try {
-					_showSubphase = typeof(EngineLoadProgress).GetField("_showSubphase", BindingFlags.NonPublic | BindingFlags.Instance);
-				} catch (Exception ex) {
-					if (!failed) {
-						Logger.WarnInternal("_showSubphase not found: " + ex.ToString());
-					}
-					failed = true;
-				}
-			}
-			return _showSubphase;
-		}
-	}
-
 	// Returned true means success, false means something went wrong.
 	internal static bool SetCustom(string text) {
 		if (ModLoaderConfiguration.Get().HideVisuals) { return true; }
-		if (!ModLoader.IsHeadless) {
-			ShowSubphase?.SetValue(Engine.Current.InitProgress, text);
-			return true;
-		}
-		return false;
+		Engine.Current.InitProgress.SetSubphase(text, true);
+		return true;
 	}
 }
 
