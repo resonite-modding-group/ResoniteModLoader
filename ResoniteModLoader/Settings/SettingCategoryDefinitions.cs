825ec36f8b947a64888e34b85977422b7506f69f
Delta <delta@deltawolf.net>
2025-08-20 05:55:33 -0700

    SettingCategoryDefinitions suppress warning about global namespace

--- ResoniteModLoader/Settings/SettingCategoryDefinitions.cs
+++ ResoniteModLoader/Settings/SettingCategoryDefinitions.cs
@@ -5,7 +5,8 @@ using FrooxEngine;
 using ResoniteModLoader.Assets;
 
 [DataModelType]
+[SuppressMessage("Design", "CA1050:Declare types in namespaces", Justification = "Needs to exist in global namespace to be used")]
 public static class SettingCategoryDefinitions {
 	[SettingCategory("ResoniteModLoader")]
-	public static SettingCategoryInfo ResoniteModLoader => new SettingCategoryInfo(RMLAssets.Icon, 0L);
+	public static SettingCategoryInfo ResoniteModLoader => new(RMLAssets.Icon, 0L);
 }
